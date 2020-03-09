51e3ccfa3f4aae25cbf8a55083681ca70a3bad0c
lubusax <lu.bu.sax@gmail.com>
2020-03-09 16:07:58 +0100

    [IAUTOSAVE] [INTEGRATE] driver into nordic code

--- main.c
+++ main.c
@@ -39,6 +39,163 @@ static const nrf_drv_twi_t m_twi = NRF_DRV_TWI_INSTANCE(TWI_INSTANCE_ID);
 /* Buffer for samples read from temperature sensor. */
 static uint8_t m_sample;
 
+//==========================================================
+// BMP280 Sensor - Specific variables and functions
+//==========================================================
+
+void    delay_ms(uint32_t period_ms);
+int8_t  i2c_reg_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length);
+int8_t  i2c_reg_read(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length);
+// void print_rslt(const char api_name[], int8_t rslt);
+
+int8_t rslt;  // error message from bme280 driver functions
+struct bmp280_dev bmp;
+
+/* Map the delay function pointer with the function responsible for implementing the delay */
+bmp.delay_ms = delay_ms;
+
+/* Assign device I2C address based on the status of SDO pin (GND for PRIMARY(0x76) & VDD for SECONDARY(0x77)) */
+bmp.dev_id = BMP280_I2C_ADDR_PRIM;
+
+/* Select the interface mode as I2C */
+bmp.intf = BMP280_I2C_INTF;
+
+/* Map the I2C read & write function pointer with the functions responsible for I2C bus transfer */
+bmp.read = i2c_reg_read;
+bmp.write = i2c_reg_write;
+
+
+/*!
+ *  @brief Function that creates a mandatory delay required in some of the APIs such as "bmg250_soft_reset",
+ *      "bmg250_set_foc", "bmg250_perform_self_test"  and so on.
+ *
+ *  @param[in] period_ms  : the required wait time in milliseconds.
+ *  @return void.
+ *
+ */
+void delay_ms(uint32_t period_ms)
+{
+    /* Implement the delay routine according to the target machine */
+    
+}
+
+/*!
+ *  @brief Function for writing the sensor's registers through I2C bus.
+ *
+ *  @param[in] i2c_addr : sensor I2C address.
+ *  @param[in] reg_addr : Register address.
+ *  @param[in] reg_data : Pointer to the data buffer whose value is to be written.
+ *  @param[in] length   : No of bytes to write.
+ *
+ *  @return Status of execution
+ *  @retval 0 -> Success
+ *  @retval >0 -> Failure Info
+ *
+ */
+int8_t i2c_reg_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length)
+{
+
+    /* Implement the I2C write routine according to the target machine. */
+    return -1;
+}
+
+/*!
+ *  @brief Function for reading the sensor's registers through I2C bus.
+ *
+ *  @param[in] i2c_addr : Sensor I2C address.
+ *  @param[in] reg_addr : Register address.
+ *  @param[out] reg_data    : Pointer to the data buffer to store the read data.
+ *  @param[in] length   : No of bytes to read.
+ *
+ *  @return Status of execution
+ *  @retval 0 -> Success
+ *  @retval >0 -> Failure Info
+ *
+ */
+int8_t i2c_reg_read(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length)
+{
+
+    /* Implement the I2C read routine according to the target machine. */
+    return -1;
+}
+
+/*!
+ *  @brief Function for writing the sensor's registers through SPI bus.
+ *
+ *  @param[in] cs           : Chip select to enable the sensor.
+ *  @param[in] reg_addr     : Register address.
+ *  @param[in] reg_data : Pointer to the data buffer whose data has to be written.
+ *  @param[in] length       : No of bytes to write.
+ *
+ *  @return Status of execution
+ *  @retval 0 -> Success
+ *  @retval >0 -> Failure Info
+ *
+ */
+int8_t spi_reg_write(uint8_t cs, uint8_t reg_addr, uint8_t *reg_data, uint16_t length)
+{
+
+    /* Implement the SPI write routine according to the target machine. */
+    return -1;
+}
+
+/*!
+ *  @brief Function for reading the sensor's registers through SPI bus.
+ *
+ *  @param[in] cs       : Chip select to enable the sensor.
+ *  @param[in] reg_addr : Register address.
+ *  @param[out] reg_data    : Pointer to the data buffer to store the read data.
+ *  @param[in] length   : No of bytes to read.
+ *
+ *  @return Status of execution
+ *  @retval 0 -> Success
+ *  @retval >0 -> Failure Info
+ *
+ */
+int8_t spi_reg_read(uint8_t cs, uint8_t reg_addr, uint8_t *reg_data, uint16_t length)
+{
+
+    /* Implement the SPI read routine according to the target machine. */
+    return -1;
+}
+
+/*!
+ *  @brief Prints the execution status of the APIs.
+ *
+ *  @param[in] api_name : name of the API whose execution status has to be printed.
+ *  @param[in] rslt     : error code returned by the API whose execution status has to be printed.
+ *
+ *  @return void.
+ */
+void print_rslt(const char api_name[], int8_t rslt)
+{
+    if (rslt != BMP280_OK)
+    {
+        printf("%s\t", api_name);
+        if (rslt == BMP280_E_NULL_PTR)
+        {
+            printf("Error [%d] : Null pointer error\r\n", rslt);
+        }
+        else if (rslt == BMP280_E_COMM_FAIL)
+        {
+            printf("Error [%d] : Bus communication failed\r\n", rslt);
+        }
+        else if (rslt == BMP280_E_IMPLAUS_TEMP)
+        {
+            printf("Error [%d] : Invalid Temperature\r\n", rslt);
+        }
+        else if (rslt == BMP280_E_DEV_NOT_FOUND)
+        {
+            printf("Error [%d] : Device not found\r\n", rslt);
+        }
+        else
+        {
+            /* For more error codes refer "*_defs.h" */
+            printf("Error [%d] : Unknown error code\r\n", rslt);
+        }
+    }
+}
+
 /**
  * @brief Function for setting active mode on MMA7660 accelerometer.
  */
@@ -133,7 +290,10 @@ int main(void)
     NRF_LOG_INFO("\r\nTWI sensor example started.");
     NRF_LOG_FLUSH();
     twi_init();
-    LM75B_set_mode();
+    //LM75B_set_mode();
+
+    rslt = bmp280_init(&bmp);
+    print_rslt(" bmp280_init status", rslt);
 
     while (true)
     {
